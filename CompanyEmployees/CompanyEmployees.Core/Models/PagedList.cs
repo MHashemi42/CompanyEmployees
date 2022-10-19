ad7efa6460ebd9fd68e81fa0a31765e196ca80c9
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2022-10-19 19:44:38 +0330

    Create PagedList for pagination

---
+++ CompanyEmployees/CompanyEmployees.Core/Models/PagedList.cs
@@ -0,0 +1,24 @@
+﻿namespace CompanyEmployees.Core.Models;
+
+public class PagedList<T> : List<T>
+{
+    public PagedListMetaData MetaData { get; init; }
+
+    public PagedList(IEnumerable<T> items, int count, int pageNumber, int pageSize)
+    {
+        MetaData = new PagedListMetaData
+        {
+            TotalCount = count,
+            PageSize = pageSize,
+            CurrentPage = pageNumber
+        };
+
+        AddRange(items);
+    }
+
+    public PagedList(IEnumerable<T> items, PagedListMetaData metaData)
+    {
+        MetaData = metaData;
+        AddRange(items);
+    }
+}
