74783bd7deabd8cdf835c1c8eed9e288c12aab57
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2022-10-19 20:10:39 +0330

    Add configurations for entities

---
+++ CompanyEmployees/CompanyEmployees.Infrastructure/Data/Configurations/CompanyConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using CompanyEmployees.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CompanyEmployees.Infrastructure.Data.Configurations;
+
+public class CompanyConfiguration : IEntityTypeConfiguration<Company>
+{
+    public void Configure(EntityTypeBuilder<Company> company)
+    {
+        company.Property(c => c.Name)
+               .HasMaxLength(100)
+               .IsRequired();
+
+        company.Property(c => c.Address)
+               .HasMaxLength(500)
+               .IsRequired();
+
+        company.Property(c => c.Country)
+               .HasMaxLength(100)
+               .IsRequired();
+    }
+}
---
+++ CompanyEmployees/CompanyEmployees.Infrastructure/Data/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using CompanyEmployees.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CompanyEmployees.Infrastructure.Data.Configurations;
+
+public class EmployeeConfiguration : IEntityTypeConfiguration<Employee>
+{
+    public void Configure(EntityTypeBuilder<Employee> employee)
+    {
+        employee.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+        employee.Property(e => e.Position)
+                .HasMaxLength(200)
+                .IsRequired();
+    }
+}
