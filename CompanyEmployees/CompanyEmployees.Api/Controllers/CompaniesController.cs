5936fc3f86596b3e842bc0da291ce7f18d98dfc2
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2022-10-19 19:37:08 +0330

    Create CompaniesController

---
+++ CompanyEmployees/CompanyEmployees.Api/Controllers/CompaniesController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CompaniesController : ControllerBase
+{
+}
