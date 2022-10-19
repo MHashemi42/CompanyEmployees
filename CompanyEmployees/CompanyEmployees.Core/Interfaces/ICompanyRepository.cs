becb9f5f274dbacda86eafe96385de499d6b95d1
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2022-10-19 20:00:16 +0330

    Create ICompanyRepository

---
+++ CompanyEmployees/CompanyEmployees.Core/Interfaces/ICompanyRepository.cs
@@ -0,0 +1,10 @@
+﻿using CompanyEmployees.Core.Entities;
+using CompanyEmployees.Core.Models;
+using CompanyEmployees.Core.Models.Parameters;
+
+namespace CompanyEmployees.Core.Interfaces;
+
+public interface ICompanyRepository
+{
+    Task<PagedList<Company>> GetCompaniesAsync(CompanyParameters companyParameters);
+}
