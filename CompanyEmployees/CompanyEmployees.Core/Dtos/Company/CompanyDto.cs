129c66bd31de9aa14a07439bcd69f25d8562873c
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2022-10-19 19:45:21 +0330

    Create CompanyDto

---
+++ CompanyEmployees/CompanyEmployees.Core/Dtos/Company/CompanyDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CompanyEmployees.Core.Dtos.Company;
+
+public class CompanyDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string FullAddress { get; set; } = null!;
+}
