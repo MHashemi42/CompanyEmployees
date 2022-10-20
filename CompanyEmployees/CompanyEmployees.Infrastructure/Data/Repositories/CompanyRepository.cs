e3aafc79c77503c184cd156d55baf01e5f5ee4d8
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2022-10-20 17:13:28 +0330

    Add CompanyRepository class

---
+++ CompanyEmployees/CompanyEmployees.Infrastructure/Data/Repositories/CompanyRepository.cs
@@ -0,0 +1,21 @@
+﻿using CompanyEmployees.Core.Entities;
+using CompanyEmployees.Core.Interfaces;
+using CompanyEmployees.Core.Models;
+using CompanyEmployees.Core.Models.Parameters;
+
+namespace CompanyEmployees.Infrastructure.Data.Repositories;
+
+public class CompanyRepository : ICompanyRepository
+{
+    private readonly AppDbContext _context;
+
+    public CompanyRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task<PagedList<Company>> GetCompaniesAsync(CompanyParameters companyParameters)
+    {
+        throw new NotImplementedException();
+    }
+}
