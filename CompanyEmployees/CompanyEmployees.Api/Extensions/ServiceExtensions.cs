afbc7507b05d2492489916511e85acf3c2b2a64d
Mohammad Hashemi <mohammadhashemi42@gmail.com>
2022-10-20 17:03:37 +0330

    Add ServiceExtensions class and AddCustomDbContext method

---
+++ CompanyEmployees/CompanyEmployees.Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using CompanyEmployees.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyEmployees.Api.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void AddCustomDbContext(this IServiceCollection services,
+        string connectionString) =>
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(connectionString));
+}
